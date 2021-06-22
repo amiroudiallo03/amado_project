f83662bbcb280f5276f5adbee83ee336becde436
amiroudiallo03 <amiroudiallo03@gmail.com>
2021-06-22 10:29:24 +0000

    commit

--- website/views.py
+++ website/views.py
@@ -2,17 +2,22 @@ from django.shortcuts import render
 
 
 def index(request):
+    is_index = True
     return render(request, 'index.html')
 
 def cart(request):
+    is_cart = True
     return render(request, 'cart.html')
 
 def checkout(request):
+    is_checkout = True
     return render(request, 'checkout.html')
 
 def product_detail(request):
+    is_product_detail = True
     return render(request, 'product-details.html')
 
 def shop(request):
+    is_shop = True
     return render(request, 'shop.html')
 # Create your views here.
