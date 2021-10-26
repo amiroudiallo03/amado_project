8fb5b0789b516621c12c4bb66585268468689e5a
amiroudiallo03 <amiroudiallo03@gmail.com>
2021-10-26 15:55:53 +0000

    last commit

--- amado_projet/settings.py
+++ amado_projet/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&e8m9toesbsbt^2jcjdd=e%#fq^_z)dhac1i6)57!=wlvya5r='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["https://amadoshop.herokuapp.com/", '127.0.0.1']
+ALLOWED_HOSTS = ["*"]
 
 # Cart
 SESSION_COOKIE_AGE = 86400
@@ -84,25 +84,25 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-	'default': {
-		'ENGINE': 'django.db.backends.postgresql',
-		'NAME': 'dfmch4n52ll6vo',
-		'USER': 'gwmpnegdmhefvw',
-		'PASSWORD': 'cc44016cc01f8d774910e137c599b3697ebdc17a53afefc59d1a530c0b5245d9',
-		'HOST': 'ec2-35-171-90-188.compute-1.amazonaws.com',
-		'PORT': 5432,
-	}
-}
-
-
-
 # DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
+# 	'default': {
+# 		'ENGINE': 'django.db.backends.postgresql',
+# 		'NAME': 'dfmch4n52ll6vo',
+# 		'USER': 'gwmpnegdmhefvw',
+# 		'PASSWORD': 'cc44016cc01f8d774910e137c599b3697ebdc17a53afefc59d1a530c0b5245d9',
+# 		'HOST': 'ec2-35-171-90-188.compute-1.amazonaws.com',
+# 		'PORT': 5432,
+# 	}
 # }
+
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # Password validation
