97faa808694572b84121e778dc3f9c3b90c7c3c4
amiroudiallo03 <amiroudiallo03@gmail.com>
2021-10-26 14:48:07 +0000

    last commit

--- amado_projet/settings.py
+++ amado_projet/settings.py
@@ -84,25 +84,25 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-# 	'default': {
-# 		'ENGINE': 'django.db.backends.postgresql',
-# 		'NAME': 'dfmch4n52ll6vo',
-# 		'USER': 'gwmpnegdmhefvw',
-# 		'PASSWORD': 'cc44016cc01f8d774910e137c599b3697ebdc17a53afefc59d1a530c0b5245d9',
-# 		'HOST': 'ec2-35-171-90-188.compute-1.amazonaws.com',
-# 		'PORT': 5432,
-# 	}
-# }
-
-
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+	'default': {
+		'ENGINE': 'django.db.backends.postgresql',
+		'NAME': 'dfmch4n52ll6vo',
+		'USER': 'gwmpnegdmhefvw',
+		'PASSWORD': 'cc44016cc01f8d774910e137c599b3697ebdc17a53afefc59d1a530c0b5245d9',
+		'HOST': 'ec2-35-171-90-188.compute-1.amazonaws.com',
+		'PORT': 5432,
+	}
 }
+
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
