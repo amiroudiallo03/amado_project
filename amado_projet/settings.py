25be296b05b10b42bca64127ebad1ab33feddfaf
amiroudiallo03 <amiroudiallo03@gmail.com>
2021-10-26 11:00:29 +0000

    heroku push

--- amado_projet/settings.py
+++ amado_projet/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-&e8m9toesbsbt^2jcjdd=e%#fq^_z)dhac1i6)57!=wlvya5r=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["amado.herokuapp.com"]
 
 # Cart
 SESSION_COOKIE_AGE = 86400
